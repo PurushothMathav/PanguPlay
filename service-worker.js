592c21e4f3bf7620896017b5ac0724b20bf85a94
PurushothMathav <purushoth.mathav@gmail.com>
2025-05-11 10:52:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "panguplay-v1";
+const CACHE_NAME = "panguplay-v51125";
 const urlsToCache = [
   "/PanguPlay/",
   "/PanguPlay/index.html",
@@ -26,5 +26,13 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
 });
