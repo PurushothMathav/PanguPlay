40daa1cb27bba7edc66f380ff92b3b196dac6815
PurushothMathav <purushoth.mathav@gmail.com>
2025-07-03 19:55:58 +0530

    v0703251900

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // PanguPlay PWA Service Worker
-const CACHE_NAME = "panguplay-v0702251000";
+const CACHE_NAME = "panguplay-v0703251900";
 const urlsToCache = [
   "/PanguPlay/",
   "/PanguPlay/index.html",
